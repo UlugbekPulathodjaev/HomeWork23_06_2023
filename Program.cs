f705d97967f0b454314f674dd0379ab0f023970e
BARINOV228 <136715066+BARINOV228@users.noreply.github.com>
2023-06-25 15:39:37 +0500

    Update Program.cs
    addde general derectory

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
-﻿using System;
+using System;
 using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string directoryPath = @"C:\CODE\forfiles";
+        string directoryPath = Path.Combine(Directory.GetCurrentDirectory(), "forfiles");
         string text;
 
         for (int i = 1; i <= 10; i++)
         {
             text = $"Файл номер {i}";
             string fileName = Path.Combine(directoryPath, $"file{i}.txt");
+
             if (File.Exists(fileName))
             {
                 Console.WriteLine($"Файл {fileName} уже существует");
