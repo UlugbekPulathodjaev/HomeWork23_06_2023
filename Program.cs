fd2903edf4509e9241af1f56057fe5651899f1a0
BARINOV <gurya2405@gmail.com>
2023-06-25 14:50:41 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string directoryPath = @"C:\CODE\forfiles";
+        string text;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            text = $"Файл номер {i}";
+            string fileName = Path.Combine(directoryPath, $"file{i}.txt");
+            if (File.Exists(fileName))
+            {
+                Console.WriteLine($"Файл {fileName} уже существует");
+                continue;
+            }
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine(text);
+            }
+        }
+    }
+}
