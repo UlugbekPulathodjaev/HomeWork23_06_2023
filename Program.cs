1c0b36925de9d7d48ac8e5c9f032634d3344377b
BARINOV <gurya2405@gmail.com>
2023-06-30 17:02:16 +0500

    Removed module for checking generated files

--- Program.cs
+++ Program.cs
@@ -5,24 +5,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        string directoryPath = Path.Combine(Directory.GetCurrentDirectory(), "forfiles");
-        string text;
-
         for (int i = 1; i <= 10; i++)
         {
-            text = $"Файл номер {i}";
-            string fileName = Path.Combine(directoryPath, $"file{i}.txt");
-
-            if (File.Exists(fileName))
-            {
-                Console.WriteLine($"Файл {fileName} уже существует");
-                continue;
-            }
-
-            using (StreamWriter writer = new StreamWriter(fileName))
-            {
-                writer.WriteLine(text);
-            }
+            string path = $"{Directory.GetCurrentDirectory()}\\file{i}.txt";
+            FileStream file = new FileStream(path, FileMode.Create);
+            file.Close();
+            Console.WriteLine($"created file {i}");
         }
     }
 }
