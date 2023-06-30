d7e1d575273d6ecf499b7e4e6301678eeb3037dc
BARINOV <gurya2405@gmail.com>
2023-06-30 17:36:14 +0500

    added class FIleCreator and two methods CreateFiles create 10 files, WriteMathToFile writes a math to a file

--- Program.cs
+++ Program.cs
@@ -5,12 +5,34 @@ class Program
 {
     static void Main(string[] args)
     {
+        FileCreator.CreateFiles();
+        FileCreator.WriteMathToFile();
+    }
+}
+
+public static class FileCreator
+{
+    public static void CreateFiles()
+    {
         for (int i = 1; i <= 10; i++)
         {
             string path = $"{Directory.GetCurrentDirectory()}\\file{i}.txt";
             FileStream file = new FileStream(path, FileMode.Create);
             file.Close();
             Console.WriteLine($"created file {i}");
+        }
+    }
+
+    public static void WriteMathToFile()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            string path = $"{Directory.GetCurrentDirectory()}\\file{i}.txt";
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine("math");
+            }
+            Console.WriteLine($"written 'math' to file {i}");
         }
     }
 }
